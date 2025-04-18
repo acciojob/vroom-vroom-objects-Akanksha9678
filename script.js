4136b2f2d47be3a6406861ed0ba0d7e73affacfc
Akanksha Kushwaha <78742130+Akanksha9678@users.noreply.github.com>
2025-04-18 23:59:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
-// Complete the js code
+// Car Constructor Function
 function Car(make, model) {
-	this.make=make;
-	this.model=model;
-	getMakeModel(){
-		return `${this.make} ${this.model}`
-	}
+  this.make = make;
+  this.model = model;
 }
 
-function SportsCar extends Car(make, model, topSpeed) {
-	super(make,model)
-	this.topSpeed=topSpeed;
-	getMakeModel(){
-		return `${this.topSpeed}`
-	}
+// Add method to Car prototype
+Car.prototype.getMakeModel = function () {
+  return `${this.make} ${this.model}`;
+};
+
+// SportsCar Constructor Function
+function SportsCar(make, model, topSpeed) {
+  
+  Car.call(this, make, model);
+  this.topSpeed = topSpeed;
 }
+SportsCar.prototype = Object.create(Car.prototype);
+
+SportsCar.prototype.constructor = SportsCar;
+
+// Add method to SportsCar prototype
+SportsCar.prototype.getTopSpeed = function () {
+  return this.topSpeed;
+};
 
 // Do not change the code below
 window.Car = Car;
